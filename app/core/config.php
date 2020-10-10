a86dda57fa31ea9f17303741fa7f7b450a32bee5
Breno Vinicius <24703386+brenovinicius@users.noreply.github.com>
2020-10-10 17:10:56 -0300

    Arquivo de configurações

---
+++ app/core/config.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+* Variaveis globais da aplicacao
+*/
+
+// Caminho raiz do projeto
+define('APP_ROOT', dirname(dirname(__FILE__)));
+
+// Nome da aplicacao
+define('APP_NAME', "Sistema de avaliações");
+
+// URL base
+define("APP_URL", "http://avaliacoes.com");
+
+// Versao
+define("APP_VERSION", "0.0.1");
