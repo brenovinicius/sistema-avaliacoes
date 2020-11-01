7a9c1d17a507f78e8b7d2eb8b14b7a99d245d557
Breno Vinicius <24703386+brenovinicius@users.noreply.github.com>
2020-11-01 19:52:13 -0300

    Opções de cor pra o aluno e sexo

---
+++ app/helpers/Cor.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Helpers;
+
+class Cor
+{
+    public static $opcoes = ["PARDA", "PRETA", "INDIGENA", "AMARELA"];
+
+}
---
+++ app/helpers/Sexo.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Helpers;
+
+class Sexo
+{
+    public static $opcoes = ["M" => "MASCULINO",
+                    "F" => "FEMININO",
+                    "O" => "OUTRO"];
+
+}
